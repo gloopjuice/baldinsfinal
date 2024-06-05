4896fe65c2be3247312f01785bd29109435f1532
gloopjuice <girts.gagainis@gmail.com>
2024-06-06 00:32:23 +0300

    sis?

--- laravelBackend/routes/api.php
+++ laravelBackend/routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
-
 Route::post('/register', [ApiController::class, 'register']);
 Route::post('/login', [ApiController::class, 'login']);
 
@@ -22,27 +21,21 @@ Route::group([
     Route::post('/createForumPost', [ApiController::class, 'createForumPost']);
     Route::get('/getForumPost', [ApiController::class, 'getForumPost']);
     Route::get('/getAllForumPosts', [ApiController::class, 'getAllForumPosts']);
-    Route::get('/post/{id}', [ApiController::class, 'show']);
-
+    Route::get('/post/{id}', [ApiController::class,'show']);
 
     Route::middleware('auth:api')->delete('/deletePost/{id}', [ApiController::class, 'deletePost']);
     Route::post('/editForumPost/{id}', [ApiController::class, 'editForumPost']);
 
-
     Route::post('/uploadFile', [ApiController::class, 'uploadFile']);
     Route::post('/deleteFile', [ApiController::class, 'deleteFile']);
 
-
     Route::get('/getAllUsers', [ApiController::class, 'getAllUsers'])->middleware('auth:api');
-
 
     Route::delete('/deleteUserProfile/{id}', [ApiController::class, 'deleteUserProfile']);
     Route::put('/updateUserProfile/{id}', [ApiController::class, 'editUserProfile']);  // Ensure this line is correct
-
 
     Route::post('/createComment', [ApiController::class, 'createComment']);
     Route::get('/getComments', [ApiController::class, 'getComments']);
     Route::put('/updateComment/{id}', [ApiController::class, 'updateComment']);
     Route::delete('/deleteComment/{id}', [ApiController::class, 'deleteComment']);
-
 });
